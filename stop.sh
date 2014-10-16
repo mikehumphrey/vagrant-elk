82ba7b53623aaeee9bd9da968b95939e3a8b1941
Michael Humphrey <mike.humphrey@mea.coop>
2014-10-16 00:17:17 -0800

    Config Updates and new Grok patterns

--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+sudo killall -9 java
 
 #  sudo service collectd stop
 #  sudo service nginx stop
-sudo /etc/init.d/elasticsearch stop
-sudo service logstash stop
+#  sudo /etc/init.d/elasticsearch stop
+#  sudo service logstash stop
